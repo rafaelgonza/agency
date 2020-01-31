23d3cfb4eddcb8c39536580bbca4c0e4dc3caf1a
rafaelgonza <rafaelgonza@gmail.com>
2020-01-31 18:27:08 +0100

    Base project

---
+++ src/main/java/com/agency/paperworkControl/PaperworkControlApplication.java
@@ -0,0 +1,13 @@
+package com.agency.paperworkControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperworkControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperworkControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agency/paperworkControl/PaperworkControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agency.paperworkControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperworkControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
